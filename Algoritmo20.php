e6b2ed8f0654476795e3ffbd657b3c04938c06f2
Maurício dos Santos Cardoso <32268735+MrMauricioritz@users.noreply.github.com>
2019-04-09 11:14:50 -0300

    Create Algoritmo20.php

---
+++ Algoritmo20.php
@@ -0,0 +1,24 @@
+    
+<?php
+
+  for($A = 0; $A < 8; $A++){ 
+    print "* * * * * * * * \n";
+}
+    print"\n";
+
+  for ($E= 0; $E < 8; $E++){
+    for ($I = -1; $I < $e; $I++){
+       print "*";
+    }
+    print"\n";
+}
+    print"\n";
+    
+  for($O = 0; $O < 8; $O++){
+    for ($U = 8; $U > $O; $U--){
+     print"*";
+    }
+    print "\n";
+}
+
+//inspiração: https://github.com/kowDown/Estudo_dirigido2/blob/master/Programa20.php //
