cf96b402124fad292c3ac5a8e974ba534f96588f
Maurício dos Santos Cardoso <32268735+MrMauricioritz@users.noreply.github.com>
2019-04-09 10:11:56 -0300

    Create Algoritmo17.php

---
+++ Algoritmo17.php
@@ -0,0 +1,10 @@
+<?php
+
+    print"escreva uma base:";
+  $UMAbase=(float)fgets(STDIN);
+
+    print"escreva um expoente:";
+  $UMexpoente=(float)fgets(STDIN);
+
+    $potencia=($UMAbase**$UMexpoente);
+  print "\n o resultado final é: $potencia";
